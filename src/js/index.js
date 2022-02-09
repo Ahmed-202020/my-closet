9799e584e8bbbf1898329c9f2141ef6fb2573011
Ahmed-202020 <ak812554@gmail.com>
2022-02-09 17:26:37 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,39 @@ $(function(){
    $(".ar").on("click" , function(){
       $("html").attr("dir" , "rtl") ;
    }) ; 
+
+   function screen_slider() {
+      var owl = $(".screen-slider");
+      owl.owlCarousel({
+          loop: true,
+          margin: 30,
+          navigation: true,
+          items: 5,
+          smartSpeed: 1000,
+          dots: true,
+          autoplay: true,
+          center: true,
+          autoplayTimeout: 2000,
+          dotsEach: true,
+          responsive: {
+              0: {
+                  items: 1
+              },
+              480: {
+                  items: 1
+              },
+              767: {
+                  items: 3
+              },
+              992: {
+                  items: 5
+              },
+              1920: {
+                  items: 5
+              }
+          }
+      });
+  }
+  screen_slider();
 });
+
